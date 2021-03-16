f38abcda40ba44e3665996939167832908006fb3
Vincent Nguyen <vpvnguyen@gmail.com>
2021-03-15 23:04:48 -0700

    replaced default react starter content with lorem placeholder

--- react-holy-grail/src/App.js
+++ react-holy-grail/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsam
+          consequuntur sequi quod maxime soluta exercitationem odio quasi vel
+          enim maiores deserunt, voluptate explicabo. Aliquid minus ut velit
+          corporis quam! Quis iusto magnam unde itaque incidunt quos omnis, quod
+          suscipit. Quo accusamus, animi maiores commodi provident ducimus
+          debitis quis quia, beatae architecto distinctio eveniet nihil
+          exercitationem impedit dolor voluptas alias. Provident ipsam porro
+          perferendis, laudantium reprehenderit sit illo? Aspernatur vitae
+          doloremque ratione unde totam maxime cum minus voluptatum. Obcaecati
+          adipisci illo sequi omnis, dolore reiciendis totam quasi ipsum
+          corrupti culpa? Atque consectetur itaque a fugit quod, velit porro
+          ipsum sunt, ab minus quidem quasi voluptatibus nesciunt? Harum
+          consectetur error animi tempora culpa porro ab similique recusandae
+          maxime, aliquam nam quam? Corporis numquam aperiam iure maxime vero at
+          quo non? Vero voluptatem doloribus perspiciatis! Magni natus accusamus
+          laudantium vitae impedit aliquid, molestias quibusdam eveniet
+          obcaecati tempore exercitationem rem ut accusantium quia.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
